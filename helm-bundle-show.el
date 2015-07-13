8ffac06a7d583968a5d3ad7fff4c5eb48f72b93b
Takashi Masuda <masutaka.net@gmail.com>
2015-07-13 22:04:20 +0900

    Bump version to 1.1.0

--- helm-bundle-show.el
+++ helm-bundle-show.el
@@ -4,7 +4,7 @@
 
 ;; Author: Takashi Masuda <masutaka.net@gmail.com>
 ;; URL: https://github.com/masutaka/emacs-helm-bundle-show
-;; Version: 1.0.1
+;; Version: 1.1.0
 ;; Package-Requires: ((helm "1.6.9"))
 
 ;; This program is free software; you can redistribute it and/or modify
