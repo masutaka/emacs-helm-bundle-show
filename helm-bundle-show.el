52d97ebeedd243e557ce17287c3ee848995b9a3e
Takashi Masuda <masutaka.net@gmail.com>
2015-08-16 21:26:28 +0900

    Require helm 1.7.6

--- helm-bundle-show.el
+++ helm-bundle-show.el
@@ -5,7 +5,7 @@
 ;; Author: Takashi Masuda <masutaka.net@gmail.com>
 ;; URL: https://github.com/masutaka/emacs-helm-bundle-show
 ;; Version: 1.1.0
-;; Package-Requires: ((helm "1.6.9"))
+;; Package-Requires: ((helm "1.7.6"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
